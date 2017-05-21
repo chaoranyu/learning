9c31bdea86e603ce952a150796e1f9176b69b703
chaoranyu <chaoranyu@gmail.com>
2017-05-21 23:07:51 +0800

    modify strsep demo

---
+++ ik-test/c/string/strsep.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int len, nel;
+    //char query[] ="username=appleboy&&&&password=123456&action=delete";
+    char query[] ="username=appleboy    password=123456 action=delete";
+    char *q, *name, *value;
+
+    /* Parse into individual assignments */
+    q = query;
+    fprintf(stdout, "CGI[query string] : %s\n", query);
+    len = strlen(query);
+    nel = 0;
+    //while (strsep(&q, "&"))
+    while (strsep(&q, " "))
+        nel++;
+    fprintf(stdout, "CGI[nel string] : %d\n\n", nel);
+
+    for (q = query; q < (query + len);) {
+        value = name = q;
+
+        /* Skip to next assignment */
+        fprintf(stdout, "CGI[str  ] : %s\n", q);
+        fprintf(stdout, "CGI[len  ] : %zu\n", strlen(q));
+        fprintf(stdout, "CGI[addr ] : %p\n", q);
+
+        for (q += strlen(q); q < (query +len) && !*q; q++);
+
+        /* Assign variable */
+        name = strsep(&value,"=");
+        fprintf(stdout, "CGI[name ] : %s\n", name);
+        fprintf(stdout, "CGI[value] : %s\n\n", value);
+    }
+
+    return 0;
+}

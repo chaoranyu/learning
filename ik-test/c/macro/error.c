ac1228cda5c8e516009d5e6220b45392bda600cf
yuchaoran <chaoranyu@gmail.com>
2017-12-16 19:16:21 +0800

    macro test

---
+++ ik-test/c/macro/error.c
@@ -0,0 +1,10 @@
+#if __STDC__ != 1
+#error "Not a standard compliant compiler"
+#endif
+
+#include <stdio.h>
+
+int main (void)
+{
+    printf("It is a Standard compiler conforming to ISO C Standard !!");
+}

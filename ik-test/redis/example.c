738549fe00773ffd1f699ea253b033da1e4154d9
yuchaoran <chaoranyu@gmail.com>
2017-12-19 10:10:47 +0800

    Add redis command test

--- ik-test/redis/example.c
+++ ik-test/redis/example.c
@@ -7,6 +7,7 @@
 
 int main(int argc, char **argv)
 {
+    unsigned int j;
     redisContext *c;
     redisReply *reply;
     const char *hostname = (argc > 1) ? argv[1] : "127.0.0.1";
@@ -58,6 +59,27 @@ int main(int argc, char **argv)
     freeReplyObject(reply);
 
     /* Create a list of numbers, from 0 to 9 */
+    reply = redisCommand(c, "DEL mylist");
+    freeReplyObject(reply);
+    for (j = 0; j < 10; j++)
+    {
+        char buf[64];
+
+        snprintf(buf, 64, "%u", j);
+        reply = redisCommand(c, "LPUSH mylist element-%s", buf);
+        freeReplyObject(reply);
+    }
+
+    /* Let's check what we have inside the list */
+    reply = redisCommand(c, "LRANGE mylist 0 -1");
+    if (reply->type == REDIS_REPLY_ARRAY)
+    {
+        for (j = 0; j < reply->elements; j++)
+        {
+            printf("%u) %s\n", j, reply->element[j]->str);
+        }
+    }
+    freeReplyObject(reply);
 
     /* Disconnects and free the context */
     redisFree(c);

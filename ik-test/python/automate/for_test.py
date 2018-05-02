225d5b414d436996c8a46c90100d617ec00ff996
yuchaoran <chaoranyu@gmail.com>
2018-05-02 19:53:16 +0800

    Exercise code of automate

---
+++ ik-test/python/automate/for_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+name = ''
+while not name:
+    print('Enter your name:')
+    name = input()
+    print('How many guests will you have?')
+    numOfGuests = int(input())
+    if numOfGuests:
+        print('Be sure to have enough room for all your guests.')
+
+print('Done')
+'''
+for i in range(5, -1, -1):
+    print(i)

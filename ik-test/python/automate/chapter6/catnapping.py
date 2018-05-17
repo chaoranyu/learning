1e5785b8cc501c11bb42a566254b78adf7823f09
yuchaoran <chaoranyu@gmail.com>
2018-05-17 21:07:04 +0800

    some code

--- ik-test/python/automate/chapter6/catnapping.py
+++ ik-test/python/automate/chapter6/catnapping.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""This is a test Python program.
+Written by Garfiled.
+This program was designed for Python 3, not Python 2.
+"""
+
 print('''Dear Alice,
 
 Eve's cat has been arrested for catnapping, cat burglary, and extortion.

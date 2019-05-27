9a68c6e9b545aecaa3e8444858c233939b1e72ac
yuchaoran <chaoranyu@gmail.com>
2019-05-27 15:22:04 +0800

    Optimize test code.

--- ik-test/c/memmove/memmove-test.c
+++ ik-test/c/memmove/memmove-test.c
@@ -2,31 +2,25 @@
 #include <string.h>
 
 typedef void* (*memfunc)(void* dest, const void* src, size_t count); 
-void test_memcopy(memfunc func, int reverse);
+void test_memcopy(memfunc func, const char *func_name, int reverse);
 
 int main()
 {
-    printf("memcpy(3, 4, 6):\n\t");
-    test_memcopy(memcpy, 0);
-
-    printf("memcpy(4, 3, 6):\n\t");
-    test_memcopy(memcpy, 1);
-
-    printf("memmove(3, 4, 6):\n\t");
-    test_memcopy(memmove, 0);
-
-    printf("memmove(4, 3, 6):\n\t");
-    test_memcopy(memmove, 1);
+    test_memcopy(memcpy, "memcpy", 0);
+    test_memcopy(memcpy, "memcpy", 1);
+    test_memcopy(memmove, "memmove", 0);
+    test_memcopy(memmove, "memmove", 1);
 
     return 0;
 }
 
-void test_memcopy(memfunc func, int reverse)
+void test_memcopy(memfunc func, const char *func_name, int reverse)
 {
     int array_a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int i;
 
-    for (i = 0; i < 10; i++)
+    printf("%s(3, 4, 6):\n\t", func_name);
+    for (i = 0; i < sizeof(array_a) / sizeof(array_a[0]); i++)
         printf("%d  ", array_a[i]);
     printf("\n\t");
 
@@ -35,7 +29,7 @@ void test_memcopy(memfunc func, int reverse)
     else
         (*func)(&array_a[4], &array_a[3], 6 * sizeof(int));
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < sizeof(array_a) / sizeof(array_a[0]); i++)
         printf("%d  ", array_a[i]);
     printf("\n");
 }

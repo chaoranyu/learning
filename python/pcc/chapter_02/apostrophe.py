ccc40e7bee032420da0a752727ed5ec79aa0efc0
chaoranyu <chaoranyu@gmail.com>
2020-12-13 23:39:13 +0800

    Add some demos of Python Crash Course.

---
+++ python/pcc/chapter_02/apostrophe.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+# string
+age = 28
+name = "  son goku  "
+message = "Hello, I'am " + name.strip().title() + ", " + str(age) + " YEARS OLD.".lower()
+print(message)
+
+# float
+f = 0.1 + 0.2
+print("0.1 + 0.2 = \t" + str(f));
+f = 0.1 * 3
+print("0.1 * 3 = \t" + str(f));
---
+++ python/pcc/chapter_03/bicycles.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized', ]
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized', 3]
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
+message = "My fourth bicycle was a " + bicycles[-1].title() + "."
+print(message)
+
+test = ['test', 321, bicycles]
+print(test)

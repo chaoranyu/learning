d6843f7e97e61c534593386ff6ddc8cab3ed74e7
chaoranyu <chaoranyu@gmail.com>
2021-05-12 17:13:01 +0800

    Add demo of SHA1

---
+++ 26-test/c/sha1-test.c
@@ -0,0 +1,45 @@
+// gcc sha1-test.c -lcrypto
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/sha.h>
+
+int main()
+{
+    SHA_CTX s;
+    int i, size;
+    uint8_t buf[8+8+4+2];
+    uint8_t res[20];
+
+    // network order
+    // abbef3bf3d51af31
+    // 0000000000000000
+    // 05010102  5.1.1.2
+    // 01f4      500
+    uint64_t ispi = 0x31af513dbff3beab;
+    uint64_t rspi = 0x0;
+    uint32_t sip = 0x2010105;
+    uint16_t sport = 0xf401;
+
+    memcpy(buf, &ispi, sizeof(ispi));
+    memcpy(buf + sizeof(ispi), &rspi, sizeof(rspi));
+    memcpy(buf + sizeof(ispi) + sizeof(rspi), &sip, sizeof(sip));
+    memcpy(buf + sizeof(ispi) + sizeof(rspi) + sizeof(sip), &sport, sizeof(sport));
+
+    for (i = 0; i < sizeof(buf); i++)
+        printf("%02x", buf[i]);
+    printf("\n");
+
+    SHA1(buf, sizeof(ispi) + sizeof(rspi) + sizeof(sip) + sizeof(sport), res);
+    // SHA1_Init(&s);
+    // size =  sizeof(ispi) + sizeof(rspi) + sizeof(sip) + sizeof(sport);
+    // SHA1_Update(&s, buf, size);
+    // SHA1_Final(res, &s);
+
+    for (i = 0; i < sizeof(res); i++)
+        printf("%02x", res[i]);
+    printf("\n");
+
+    return 0;
+}

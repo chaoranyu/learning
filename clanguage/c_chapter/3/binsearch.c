1b93bb910b57086687ed1a73bb07fbfa3dcbf5ee
chaoranyu <chaoranyu@gmail.com>
2021-06-16 15:53:25 +0800

    remove duplicated code

--- clanguage/c_chapter/3/binsearch.c
+++ clanguage/c_chapter/3/binsearch.c
@@ -19,24 +19,6 @@ int binsearch(int x, int v[], int n)
 	return -1; /* no match */
 }
 
-int binsearch(int x, int v[], int n)
-{
-	int low, high, mid;
-	low = 0;
-	high = n - 1;
-	while (low <= high) {
-		mid = (low+high)/2;
-		if (x < v[mid])
-			high = mid - 1; //high = mid + 1;
-		else if (x > v[mid])
-			low = mid + 1;
-		else
-			/* found match */
-			return mid;
-	}
-	return -1;
-}
-
 void main()
 {
 	int v[10] = {1, 3, 5, 7, 9, 11, 23, 35, 47, 59};
